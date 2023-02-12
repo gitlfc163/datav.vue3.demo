e803e3724de9d0aa9ac7a7adbe9dd3d8b59bf179
longfc <longfuchu@163.com>
2023-02-12 18:09:03 +0800

    init

---
+++ src/components/DataVDemo.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { BorderBox1 } from '@dataview/datav-vue3';
+</script>
+
+<template>
+    <BorderBox1 class="container"> Content </BorderBox1>
+</template>
+
+<style>
+.container {
+    width: 500px;
+    height: 200px;
+}
+</style>
